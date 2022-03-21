c041b97833c6c9ca425330d72b7c75a9aeef97e2
Wilson Luz <wluzempreendimentos@outlook.com>
2022-03-20 22:41:56 -0300

    first commit

---
+++ git.js
@@ -0,0 +1,12 @@
+//Entrar na pasta:
+// git init
+// git config user.name "Wilson Luz"
+// git config user.email "wluzempreendimentos@outlook.com"
+// git remote add origin "https://github.com/usuario/arquivo.git"
+
+//Mandar arquivo para git
+//git add arquivo.js
+//git status
+//git commit -m "texto"
+//git push -u origin master
+
